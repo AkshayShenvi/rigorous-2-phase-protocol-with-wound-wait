d408bbf1680245ce2bcf53ed2268b9bcc3066685
Yash Bardapurkar <ybardapurkar@gmail.com>
2019-07-23 12:11:19 -0500

    begin function

--- project1.py
+++ project1.py
@@ -1,3 +1,6 @@
+transaction_list = []
+locks_dict = {}
+
 def checkoperation(opr):
     operation = 'invalid'
     transaction_num = 'invalid'
@@ -18,20 +21,33 @@ def checkoperation(opr):
         transaction_num = opr[1]
 
     return operation,transaction_num,item
+
 def checkchar(input):
     input_ops=input.split("\n")
     operation=[]
 
-    for n,i in enumerate(input_ops):
-        temp_clean=i.replace(" ","")
-        input_ops[n]=temp_clean
-
+    for n, i in enumerate(input_ops):
+        temp_clean = i.replace(" ","")
+        input_ops[n] = temp_clean
 
     for i in input_ops:
         operation.append(checkoperation(i))
     print(operation)
 
+    for op in operation:
+        if op[0] == 'begin':
+            begin(op[1])
 
+def begin(transaction_id):
+    new_transaction = {}
+    new_transaction['transaction_id'] = transaction_id
+    new_transaction['timestamp'] = 0
+    new_transaction['state'] = 'active'
+    new_transaction['item_list'] = []
+
+    transaction_list.append(new_transaction)
+
+    print('new transaction started: ' + str(new_transaction))
 
 
 if __name__=='__main__':
