1f50da3165ec4749b80d75f91dcc96a11c537c8c
Akshay Shenvi <akshayshenvi@gmail.com>
2019-07-26 15:19:23 -0500

    final with output, requirements and readme files

---
+++ Readme.txt
@@ -0,0 +1,13 @@
+check requirements.txt for requirements
+
+
+run this command:python project1.py <name of input file in the same folder>
+
+You will get an output file with the name output_of_<name of input file>
+
+eg.
+if you run:
+python project1.txt input1.txt
+
+you will get:
+output_of_input1.txt
--- project1.py
+++ project1.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import sys
 
-input_file = "input6.txt"
-output_file = "output_" + input_file
+#input_file = "input6.txt"
+
 
 timestamp = 0
 lock_table = pd.DataFrame(columns=["transaction_id", "data_item", "state"])
@@ -456,6 +457,160 @@ def end(transaction_id):
         return True
 
 
+def wait_write(transaction_id, data_item):
+
+    global timestamp
+    global transaction_table
+    global lock_table
+    global wait_list
+    global output_list
+    global in_waitlist
+
+    # current transaction is active
+    if transaction_table[transaction_table["transaction_id"] == transaction_id]["state"].values[0] == "active":
+        
+        # lock by another transaction, wound wait
+        if len(lock_table[(lock_table["data_item"] == data_item) 
+                & (lock_table["transaction_id"] != transaction_id)]) > 0:
+
+            # get conflicting lock
+            conflicting_lock = lock_table[(lock_table["transaction_id"] != transaction_id) & (lock_table["data_item"] == data_item)]
+
+            # get conflcting transaction
+            conflicting_transaction_id = conflicting_lock["transaction_id"].values[0]
+
+            wound_wait_result = wound_wait(transaction_id, conflicting_transaction_id)
+
+            # apply read lock to requesting transaction if locking transaction is aborted
+            message = ""
+            if wound_wait_result == 'abort':
+
+                # release all locks held by the transaction
+                locks_to_release = str(lock_table[lock_table["transaction_id"] == conflicting_transaction_id]["data_item"].values)[1:-1]
+                lock_table = lock_table[lock_table["transaction_id"] != conflicting_transaction_id]
+
+                # apply write lock
+                lock_index = lock_table[(lock_table["transaction_id"] == transaction_id) & (lock_table["data_item"] == data_item)].index
+                if len(lock_index.values) == 0:
+                    # dirty write
+                    lock = {"transaction_id": transaction_id, "data_item": data_item, "state": "write"}
+                    lock_table = lock_table.append(lock, ignore_index=True)
+                else:
+                    lock_table.at[lock_index, "state"] = "write"
+
+                # set message string
+                message = "Abort Transaction " + str(conflicting_transaction_id) + " and release locks on " + locks_to_release + ", acquire write lock on " + data_item
+            else:
+                
+                # add blocked operation to wait_list
+                wait_list.append(("write", transaction_id, data_item))
+
+                # set message string
+                message = "Block Transaction " + str(transaction_id) + ", add operation to wait list"
+
+            # append to output file
+            output = "w" + str(transaction_id) + "(" + data_item + ");  :Transaction " + str(transaction_id) + ": Requesting write lock on " + data_item + ": wound wait : " + message
+            if in_waitlist:
+                output = output + ' (from waitlist)'
+            print(output)
+            output_list.append(output)
+
+            # run waitlisted transactions if aborted
+            if wound_wait_result == 'abort':
+                run_wait_list()
+
+            return False
+
+        # exclusive lock by self, write
+        elif len(lock_table[(lock_table["data_item"] == data_item)
+                & (lock_table["transaction_id"] == transaction_id)
+                & (lock_table["state"] == "write")]) > 0:
+            
+            # append to output file
+            output = "w" + str(transaction_id) + "(" + data_item + ");  :Transaction " + str(transaction_id) + ": write " + data_item
+            if in_waitlist:
+                output = output + ' (from waitlist)'
+            print(output)
+            output_list.append(output)
+
+            return True
+        
+        # no exclusive lock, okay to lock and write
+        else:
+
+            # append to output file
+            output = "w" + str(transaction_id) + "(" + data_item + ");  :Transaction " + str(transaction_id) + ": acquire write lock on " + data_item
+            if in_waitlist:
+                output = output + ' (from waitlist)'
+            print(output)
+            output_list.append(output)
+
+            # apply write lock
+            lock_index = lock_table[(lock_table["transaction_id"] == transaction_id) & (lock_table["data_item"] == data_item)].index
+            if len(lock_index.values) == 0:
+                # dirty write
+                lock = {"transaction_id": transaction_id, "data_item": data_item, "state": "write"}
+                lock_table = lock_table.append(lock, ignore_index=True)
+            else:
+                lock_table.at[lock_index, "state"] = "write"
+
+            return True
+
+    # current transaction is waiting
+    elif transaction_table[transaction_table["transaction_id"] == transaction_id]["state"].values[0] == "waiting":
+        
+        # append to output file
+        output = "w" + str(transaction_id) + "(" + data_item + ");  :Transaction " + str(transaction_id) + ": waiting, added operation to waiting list"
+        if in_waitlist:
+            output = output + ' (from waitlist)'
+        print(output)
+        output_list.append(output)
+
+        # add to wait_list
+        wait_list.append(('write', transaction_id, data_item))
+
+        return False
+    
+    # current transaction is aborted
+    elif transaction_table[transaction_table["transaction_id"] == transaction_id]["state"].values[0] == "aborted":
+        
+        # append to output file
+        output = "w" + str(transaction_id) + "(" + data_item + ");  :Transaction " + str(transaction_id) + " is aborted"
+        if in_waitlist:
+            output = output + ' (from waitlist)'
+        print(output)
+        output_list.append(output)
+
+        return True
+    
+    # current transaction is completed
+    elif transaction_table[transaction_table["transaction_id"] == transaction_id]["state"].values[0] == "completed":
+        
+        # append to output file
+        output = "w" + str(transaction_id) + "(" + data_item + ");  :Transaction " + str(transaction_id) + " is completed"
+        if in_waitlist:
+            output = output + ' (from waitlist)'
+        print(output)
+        output_list.append(output)
+
+        return True
+
+    # current transaction has invalid state
+    else:
+
+        # append to output file
+        output = "w" + str(transaction_id) + "(" + data_item + ");  :Transaction " + str(transaction_id) + " state is invalid"
+        if in_waitlist:
+            output = output + ' (from waitlist)'
+        print(output)
+        output_list.append(output)
+
+        return True
+
+
+
+
+
 def wound_wait(requesting_transaction_id, locking_transaction_id):
 
     global transaction_table
@@ -530,7 +685,7 @@ def run_wait_list():
                 wait_list = wait_list_copy[:-1]
                 break
         elif op[0] == 'write':
-            if not write(int(op[1]), op[2]):
+            if not wait_write(int(op[1]), op[2]):
                 wait_list = wait_list_copy[:-1]
                 break
         elif op[0] == 'end':
@@ -544,62 +699,67 @@ def run_wait_list():
     # print('----------------------------------------')
 
 if __name__=='__main__':
-    output_list
-    file=open(input_file, "r+")
-    input=file.read()
-    input_ops = checkchar(input)
+    if len(sys.argv) == 2:
+        output_list
+        input_file=sys.argv[1]
+        output_file = "output_of_" + input_file
+        file=open(input_file, "r+")
+        input=file.read()
+        input_ops = checkchar(input)
 
-    for op in input_ops:
-        op = checkoperation(op)
+        for op in input_ops:
+            op = checkoperation(op)
 
-        if op[0] == 'begin':
-            begin(int(op[1]))
-        elif op[0] == 'read':
-            read(int(op[1]), op[2])
-        elif op[0] == 'write':
-            write(int(op[1]), op[2])
-        elif op[0] == 'end':
-            end(int(op[1]))
+            if op[0] == 'begin':
+                begin(int(op[1]))
+            elif op[0] == 'read':
+                read(int(op[1]), op[2])
+            elif op[0] == 'write':
+                write(int(op[1]), op[2])
+            elif op[0] == 'end':
+                end(int(op[1]))
 
-    # execute any operations if remaining
-    run_wait_list()
-    
-    print()        
-    print("transaction_table: ")
-    print(transaction_table)
-    
-    print()
-    print("lock_table: ")
-    print(lock_table)
-    
-    print()
-    print("wait_list: ")
-    print(wait_list)
-    #lines=[]
+        # execute any operations if remaining
+        run_wait_list()
+        
+        print()        
+        print("transaction_table: ")
+        print(transaction_table)
+        
+        print()
+        print("lock_table: ")
+        print(lock_table)
+        
+        print()
+        print("wait_list: ")
+        print(wait_list)
+        #lines=[]
 
-    #print((input_ops))
-    #print((output_list))
-    with open(output_file, 'w') as f:
-        for item in output_list:
-            f.write("%s\n" % item)
+        #print((input_ops))
+        #print((output_list))
+        with open(output_file, 'w') as f:
+            for item in output_list:
+                f.write("%s\n" % item)
 
-    file1 = open(output_file, "a+")
-    file1.write("\n*number of output operations may be greater than number of input operations \n because operations that are waiting are started once \n the locks are acquired")
-    file1.close()
-    # for i in range(len(output_list)):
-    #     lines.append(input_ops[i]+":"+output_list[i])
-    # final_string = '\n'.join(lines)
-    # print(final_string)
-    # with open('output.txt', 'w') as f:
-    #     for item in lines:
-    #         f.write("%s\n" % item)
+        file1 = open(output_file, "a+")
+        file1.write("\n*number of output operations may be greater than number of input operations \n because operations that are waiting are started once \n the locks are acquired")
+        file1.close()
+        # for i in range(len(output_list)):
+        #     lines.append(input_ops[i]+":"+output_list[i])
+        # final_string = '\n'.join(lines)
+        # print(final_string)
+        # with open('output.txt', 'w') as f:
+        #     for item in lines:
+        #         f.write("%s\n" % item)
 
 
 
-    # # print( transaction_table[(transaction_table["transaction_id"] > 1) & (transaction_table["timestamp"] < 3)] )
-    # print('detete transaction 1')
-    # print(transaction_table[transaction_table["transaction_id"] == 1].index)
-    # print(transaction_table)
+        # # print( transaction_table[(transaction_table["transaction_id"] > 1) & (transaction_table["timestamp"] < 3)] )
+        # print('detete transaction 1')
+        # print(transaction_table[transaction_table["transaction_id"] == 1].index)
+        # print(transaction_table)
 
-    file.close()
+        file.close()
+    else:
+        print("Invalid number of arguments. Argument should be: python project1.py <name of inputfile in same folder> ")
 
